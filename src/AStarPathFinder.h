661980fbd8f9e018fe720d2fee759354b8f935f1
Matt Chambers <mvchambers@me.com>
2011-08-04 22:42:45 -0700

    first commit

---
+++ src/AStarPathFinder.h
@@ -0,0 +1,112 @@
+/*
+ * AStarPathfinder https://github.com/sqlboy/tiled-games
+ *
+ * Copyright (c) 2011 Matt Chambers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+#define ASTAR_COLLIDE_PROP_NAME @"COLLIDE"
+#define ASTAR_COLLIDE_PROP_VALUE @"1"
+#define ASTAR_PATH_COLOR 0.2, 0.5, 0.2, 0.3
+
+@class AStarNode;
+@class AStarPathFinder;
+
+/**
+* A structure for storing a A* node
+*/
+@interface AStarNode : NSObject {
+  int x;
+  int y;
+@public
+  AStarNode *parent;
+  CGPoint point;
+  int F;
+  int G;
+  int H;
+}
+
+/**
+* Create a new autoreleased node at the given tile position.
+*/
++ (id) nodeAtPoint:(CGPoint)pos;
+
+/**
+* Initialize the node at the given tile position;
+**/
+- (id) initAtPoint:(CGPoint)pos;
+
+/**
+* Returns the calculated cost of the node.
+*/
+- (int) cost;
+
+
+@end
+
+/**
+* AStarPathFinder provides the the ability to animate sprites around
+* a CCTMXTiledMap along an A* path calcuated by suppied source
+* and destination tiles.
+**/
+@interface AStarPathFinder : CCLayer {
+  CCTMXTiledMap *tileMap;
+  CCTMXLayer *collideLayer;
+  NSMutableSet *openNodes;
+  NSMutableSet *closedNodes;
+  NSString *collideKey;
+  NSString *collideValue;
+}
+
+/** The name of the tile property which stores the collision boolean. */
+@property (copy, nonatomic) NSString *collideKey;
+/** The value of the tile property which indicates a collide tile. */
+@property (copy, nonatomic) NSString *collideValue;
+
+/**
+* Initialize the object with a CCTMXTileMap and the name of
+* the layer which contains your collision tiles.
+* 
+* The default collide property name is COLLIDE,
+* which is checked for the default value of 1.  Use setCollideKey
+* and setCollideValue to customize.
+*/
+- (id) initWithTileMap:(CCTMXTiledMap*)aTileMap collideLayer:(NSString*)name;
+
+/**
+* Highlight the calculated A* path.
+**/
+- (void) highlightPathFrom:(CGPoint)src to:(CGPoint)dst;
+
+/**
+* Move given sprite along the calcualted A* path.
+**/
+- (void) moveSprite:(CCSprite*)sprite 
+        from:(CGPoint)src to:(CGPoint)dst atSpeed:(float)speed;
+
+@end
+
+
+
+
---
+++ src/AStarPathFinder.m
@@ -0,0 +1,291 @@
+/*
+ * AStarPathfinder https://github.com/sqlboy/tiled-games
+ *
+ * Copyright (c) 2011 Matt Chambers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+#import "AStarPathFinder.h"
+#import "cocos2d.h"
+
+@interface AStarPathFinder (Private)
+  - (AStarNode *) lowCostNode;
+  - (BOOL) isCollision:(CGPoint)point;
+  - (AStarNode *) findPathFrom:(CGPoint)src to:(CGPoint)dst;
+  - (CGImageRef) makePathTile;
+@end
+
+@implementation AStarPathFinder
+@synthesize collideKey;
+@synthesize collideValue;
+
+- (id) initWithTileMap:(CCTMXTiledMap*)aTileMap collideLayer:(NSString*)name
+{
+  if ((self=[super init])) 
+  {
+    tileMap = [aTileMap retain];
+    openNodes = [[NSMutableSet setWithCapacity:16] retain];
+    closedNodes = [[NSMutableSet setWithCapacity:64] retain];
+    collideLayer = [tileMap layerNamed:name];
+    collideKey = ASTAR_COLLIDE_PROP_NAME;
+    collideValue = ASTAR_COLLIDE_PROP_VALUE;
+  }
+
+  return self;
+}
+
+- (void) dealloc
+{
+  [tileMap release];
+  [openNodes release];
+  [closedNodes release];
+  [super dealloc];
+}
+
+- (CGImageRef) makePathTile
+{
+  int width = [tileMap tileSize].width;
+  int height = [tileMap tileSize].height;
+
+  CGContextRef context = NULL;
+  CGColorSpaceRef imageColorSpace = CGColorSpaceCreateDeviceRGB();
+  
+  context = CGBitmapContextCreate(NULL, width, 
+    height, 8, width * 4, imageColorSpace, kCGImageAlphaPremultipliedLast);
+  
+  CGContextSetRGBFillColor(context, ASTAR_PATH_COLOR);
+  CGContextFillRect(context, CGRectMake(0, 0, width, height));
+  
+  CGImageRef imgRef = CGBitmapContextCreateImage(context);
+  return imgRef;
+}
+  
+- (AStarNode *) findPathFrom:(CGPoint)src to:(CGPoint)dst
+{
+  [self removeAllChildrenWithCleanup:YES];
+
+  [openNodes removeAllObjects];
+  [closedNodes removeAllObjects];
+
+  if ([self isCollision:dst]) {
+    return nil;
+  }
+
+  AStarNode *origin = [AStarNode nodeAtPoint:src];
+  origin->parent = nil;
+  [openNodes addObject:origin];
+
+  AStarNode *closestNode = nil;
+  while ([openNodes count])
+  {
+    closestNode = [self lowCostNode];
+    if (closestNode->point.x == dst.x && closestNode->point.y == dst.y)
+    {
+      return closestNode;
+    }
+    else
+    {
+      [openNodes removeObject:closestNode];
+      [closedNodes addObject:closestNode];
+      
+      int currentX = closestNode->point.x;
+      int currentY = closestNode->point.y;
+      
+      for (int y=-1; y<=1; y++)
+      {
+        for(int x=-1; x<=1; x++)
+        {
+          // Skip over self
+          if (x == 0 && y == 0)
+            continue;
+          
+          AStarNode *adjacentNode = [AStarNode
+            nodeAtPoint:ccp(currentX+x, currentY+y)];
+          adjacentNode->parent = closestNode;
+          
+          // Skip over this node if its already been closed.
+          if ([closedNodes containsObject:adjacentNode])
+            continue;
+   
+          // Skip over collide nodes, and add them to the closed set.
+          if ([self isCollision:adjacentNode->point]) {
+            [closedNodes addObject:adjacentNode];
+            continue;
+          }
+
+          // Calculate G
+          // G cost is 10 for adjacent and 14 for a diagonal move.
+          // We use these numbers because the distance to move diagonally
+          // is the square root of 2, or 1.414 the cost of moving
+          // horizontally or vertically.
+          if (abs(x) == 1 && abs(y) == 1)
+            adjacentNode->G = 14;
+          else
+            adjacentNode->G = 10;
+
+          // Calculate H
+          // Uses 'Mahhattan' method wich is just the number
+          // of horizonal and vertical hops to the target.
+          adjacentNode->H = abs(adjacentNode->point.x - dst.x) +
+            abs(adjacentNode->point.y - dst.y);
+
+          [openNodes addObject:adjacentNode];
+        }
+      }
+    }
+  }
+  return nil;
+}
+
+
+- (void) highlightPathFrom:(CGPoint)src to:(CGPoint)dst 
+{
+  AStarNode *node = [self findPathFrom:src to:dst];
+  if (node == nil)
+    return;
+
+  // TODO: allow people to chnage the color of the highlight path.
+  CGImageRef pathImage = [self makePathTile];
+  while(node != nil)
+  {
+    CGPoint p1 = [collideLayer
+      positionAt:node->point];
+    p1.x = p1.x + 16;
+    p1.y = p1.y + 16;
+    
+    CCSprite *spr = [CCSprite spriteWithCGImage:pathImage key:@"T"];
+    spr.position = p1;
+    [self addChild:spr];
+    node = node->parent;
+  }
+}
+
+
+- (void) moveSprite:(CCSprite*)sprite 
+         from:(CGPoint)src to:(CGPoint)dst atSpeed:(float)speed
+{
+  AStarNode *node = [self findPathFrom:src to:dst];
+  if (node == nil)
+    return;
+
+  NSMutableArray *actionList = [NSMutableArray array];
+  while(node != nil) 
+  {
+    CGPoint p1 = [collideLayer
+      positionAt:node->point];
+    p1.x = p1.x + 16;
+    p1.y = p1.y + 16;
+    
+    CCAction *move = [CCMoveTo actionWithDuration: speed position: p1];
+    [actionList addObject:move];
+    node = node->parent;
+  }
+  
+  NSArray* reversedArray = [[actionList reverseObjectEnumerator] allObjects];
+  [sprite runAction:[CCSequence actionsWithArray:reversedArray]];
+}
+
+
+- (BOOL) isCollision:(CGPoint)point
+{
+  // Check for a tile in the collide layer.
+  UInt32 tileGid = [collideLayer tileGIDAt:point];
+  if (tileGid)
+  {
+    // If a tile exists, see if collide is enabled on the entire layer.
+    NSDictionary *ldict = [collideLayer propertyNamed:collideKey];
+    if (ldict)
+      return YES;
+
+    // If not, then check the tile for the collide property.
+    NSDictionary *dict = [tileMap propertiesForGID:tileGid];
+    if (dict)
+    {
+      NSString *collide = [dict valueForKey:collideKey];
+      if (collide && [collide compare:collideValue] == NSOrderedSame)
+        return YES;
+    }
+  }
+  return NO;
+}
+
+- (AStarNode *) lowCostNode
+{  
+  AStarNode *lowCostNode = [openNodes anyObject];
+  for (id setObject in openNodes)
+  {
+    if ([(AStarNode*)setObject cost] < [lowCostNode cost])
+    {
+      lowCostNode = (AStarNode*)setObject;
+    }
+  }
+  return lowCostNode;
+}
+
+@end
+
+@implementation AStarNode
+
++ (id) nodeAtPoint:(CGPoint)point;
+{
+  return [[[AStarNode alloc] initAtPoint:point] autorelease];
+}
+
+- (id) initAtPoint:(CGPoint)pnt
+{
+  point = pnt;
+  x = pnt.x;
+  y = pnt.y;
+  return self;
+}
+
+- (void) dealloc {
+  parent = nil;
+  [super dealloc];
+}
+
+- (int) cost {
+  return G + H;
+}
+
+- (NSUInteger) hash
+{
+  return (x << 16) | (y & 0xFFFF);
+}
+
+- (BOOL)isEqual:(id)otherObject
+{
+  if (![otherObject isKindOfClass:[self class]])
+  {
+    return NO;
+  }
+  
+  AStarNode *otherNode = (AStarNode*) otherObject;
+  if (point.x == otherNode->point.x && point.y == otherNode->point.y)
+  {
+    return YES;
+  }
+  
+  return NO;
+}
+
+@end
+
